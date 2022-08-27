7e3cb397fdcb659ced7e213191ce5a72e4154825
m.kamaei <m.kamaei>
2022-08-27 22:13:20 +0430

    add apiGw and rabit mq

---
+++ amqp/src/main/java/com/amigoscode/amqp/RabbitMqConfig.java
@@ -0,0 +1,15 @@
+package com.amigoscode.amqp;
+
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+//    private  final ConnectionFactory connectionFactory;
+//    public AmqpTemplate amqpTemplate(){
+//
+//        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+//    }
+}
